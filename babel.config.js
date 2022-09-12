615567a6d0ac8bd5ebbfdd560b5b27f037baa39e
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2022-09-12 11:09:08 -0300

    chore: Setup absolute paths importing modules

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          extensions: ['.js', '.jsx', '.es', '.es6', '.mjs', '.ts', '.tsx', '.svg', '.png', '.cjs'],
+          root: ['./'],
+          alias: {
+            '@modules': './src/modules',
+            '@shared': './src/shared',
+          }
+        }
+      ]
+    ]
   };
 };
