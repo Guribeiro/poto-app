2b756c15bd1367331caa055d5bb5bed25e87c8e7
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2022-09-19 16:43:22 -0300

    chore: add touchable

---
+++ src/shared/common/components/Touchable/index.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, cloneElement, createElement } from 'react';
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+
+export const Container = styled(TouchableOpacity)`
+  padding: ${({ theme }) => theme.screen.rem(.5)}px;
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.palette.colors.white};
+  font-size: ${({ theme }) => theme.screen.rem(1.4, true)}px;
+`;
+
+interface TouchableProps extends TouchableOpacityProps {
+  children?: ReactNode;
+  text?: string;
+}
+
+
+const Touchable = ({children, text, ...props}: TouchableProps): JSX.Element => {
+  return (
+    <Container {...props}>
+        {children || text}
+    </Container>
+  )
+}
+
+export default Touchable;
