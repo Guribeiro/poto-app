931d6cfda747f19e29fa592577ca68a6fb4cb021
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2022-09-22 08:25:16 -0300

    chore: add tabbar

---
+++ src/modules/app/components/Tabbar/index.tsx
@@ -0,0 +1,90 @@
+import { connect } from 'react-redux';
+import { Dispatch, bindActionCreators } from 'redux';
+import {
+  BottomTabBarProps,
+  BottomTabNavigationOptions,
+} from '@react-navigation/bottom-tabs';
+import { Container, Touchable, Icon, UserAvatar } from './styles';
+import { ApplicationState } from '@shared/store';
+import { AuthenticationState } from '@shared/store/ducks/authentication/types';
+
+
+type TabBarStyle = {
+  display: 'none' | 'flex';
+};
+
+type RoutesNameVariations = {
+  [key: string]: {
+    name: string;
+    icon: string;
+  };
+};
+
+const routesNameVariations: RoutesNameVariations = {
+  Feed: {
+    name: 'Loja',
+    icon: 'home',
+  },
+  ProfileRoutes: {
+    name: 'Perfil',
+    icon: 'user',
+  },
+  PostForm: {
+    name: 'Post',
+    icon: 'user',
+  },
+};
+
+interface StateProps {
+  authentication: AuthenticationState;
+}
+
+type TabBarProps = BottomTabBarProps & StateProps;
+
+
+const TabBar = ({
+  navigation,
+  descriptors,
+  state,
+  authentication
+}: TabBarProps): JSX.Element => {
+  const { data } = authentication;
+  const { user } = data;
+
+  const uri = user.avatar ?
+    `http://10.0.0.175:3333/files/avatars/${user.avatar}` :
+    `https://ui-avatars.com/api/?name=${user.full_name}&length=1`;
+
+  const routes = Object.keys(descriptors).map(item => descriptors[item].route);
+
+  const focusedOptions = descriptors[state.routes[state.index].key]
+    .options as BottomTabNavigationOptions;
+
+  return (
+    <Container>
+      {routes.map((route, index) => {
+        const { icon, name } = routesNameVariations[route.name];
+
+        return (
+          <Touchable
+            key={route.key}
+            onPress={() => navigation.navigate(route.name)}
+            selected={index === state.index}
+          >
+            {name === 'Perfil' ? (
+              <UserAvatar source={{ uri }} />
+            ) : (
+              <Icon name={icon} selected={index === state.index} />
+            )}
+          </Touchable>
+        );
+      })}
+    </Container>
+  );
+};
+
+const mapStateToProps = ({ authentication }: ApplicationState) => ({
+  authentication
+})
+
+export default connect(mapStateToProps)(TabBar)
---
+++ src/modules/app/components/Tabbar/styles.ts
@@ -0,0 +1,62 @@
+import styled, { css } from 'styled-components/native';
+import { darken } from 'polished';
+import { Feather } from '@expo/vector-icons';
+import { Image, View, TouchableOpacity } from 'react-native';
+
+import { Text } from '@shared/common/components/Text';
+
+type SelectedProps = {
+  selected: boolean;
+};
+
+
+
+export const Container = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+
+  background: ${({ theme }) => theme.palette.colors.secondary};
+  border-top-width: 0.5px;
+  border-top-color: ${({ theme }) => theme.palette.colors.texts.strong};
+
+`;
+
+export const Touchable = styled(TouchableOpacity) <SelectedProps>`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  height: ${({ theme }) => theme.screen.rem(3.125)}px;
+
+  ${({ selected }) => selected && css`
+    background: ${({ theme }) => darken(0.05, theme.palette.colors.secondary)};
+  `}
+
+`;
+
+export const TouchableText = styled(Text) <SelectedProps>`
+  font-family: ${({ theme }) => theme.palette.fonts.bold};
+  font-size: ${({ theme }) => theme.screen.rem(1, true)}px;
+  margin-top: ${({ theme }) => theme.screen.rem(0.1)}px;
+
+  color: ${({ theme, selected }) =>
+    selected
+      ? theme.palette.colors.white
+      : theme.palette.colors.texts.medium};
+`;
+
+export const Icon = styled(Feather) <SelectedProps>`
+  font-size: ${({ theme }) => theme.screen.rem(1.6, true)}px;
+
+  color: ${({ theme, selected }) =>
+    selected
+      ? theme.palette.colors.shapes.strong
+      : theme.palette.colors.texts.light};
+`;
+
+
+export const UserAvatar = styled(Image)`
+  width: ${({ theme }) => theme.screen.rem(2)}px;
+  height: ${({ theme }) => theme.screen.rem(2)}px;
+  border-radius: ${({ theme }) => theme.screen.rem(1)}px;
+`;
